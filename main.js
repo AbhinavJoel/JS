b9f0b87f038fbff53f2b5070776cc2fc0776250d
AbhinavJoel <abhijoe20@gmail.com>
2020-09-02 02:48:23 +0530

    regexp practice

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
 
-
-function multiply(a, b = 1) {
-    return a * b;
-  }
-  
-  console.log(multiply(5, 2));
-  
-  console.log(multiply(10,5));
-  
-
-  function toCelsius(fahrenheit) {
-    return (5/9) * (fahrenheit-32);
-  }
-  console.log(toCelsius(56))
-  
+    var x = document.getElementById("regexp").innerHTML;
+    console.log(x)
+    var patt = /B/i;
+    var result = x.match(patt);
+    console.log(result)
