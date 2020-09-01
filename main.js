71bfe737ba16f14bcc25c28f3ce87780a730f7ca
AbhinavJoel <abhijoe20@gmail.com>
2020-09-02 01:19:12 +0530

    Total and avg js file

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-let age = 30;
-const num = 500;
-console.log(age);
-console.log(num);
-console.log(age==num)
-
-let array=[1,2,3,4,5]
-numb = array[3]
-console.log(String(numb))
+var x = document.getElementById("marks1").innerHTML;
+var y = document.getElementById("marks2").innerHTML;
+a= parseInt(x)
+b= parseInt(y)
+total = a+b
+console.log(total)
+avg= total/2
+console.log(avg)
+document.getElementById('total').innerHTML = total
+document.getElementById('avg').innerHTML = avg
