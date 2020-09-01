cd6ed8cebab2adacdd6d45e07932f0de24258c61
AbhinavJoel <abhijoe20@gmail.com>
2020-09-02 02:55:34 +0530

    Math

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-
-    var x = document.getElementById("regexp").innerHTML;
-    console.log(x)
-    var patt = /B/i;
-    var result = x.match(patt);
-    console.log(result)
+var  x = Math.pow(4, 2);
+console.log(x)
+x = Math.sqrt(100);
+console.log(x)
+x = Math.abs(9.82);  
+console.log(x)
+x = Math.random();  
+console.log(x)
+x = Math.ceil(9.8); 
+console.log(x)
+x = Math.round(6.3);
+console.log(x)
+x = Math.floor(8.9);  
+console.log(x)
+x = Math.sin(90 * Math.PI / 180);  
+console.log(x)
