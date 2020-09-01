c43f99077568d9112c772d6c5ca4893a1bc28df4
AbhinavJoel <abhijoe20@gmail.com>
2020-09-02 01:37:57 +0530

    functions added

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-var x = document.getElementById("marks1").innerHTML;
-var y = document.getElementById("marks2").innerHTML;
-a= parseInt(x)
-b= parseInt(y)
-total = a+b
-console.log(total)
-avg= total/2
-console.log(avg)
-document.getElementById('total').innerHTML = total
-document.getElementById('avg').innerHTML = avg
+
+
+function multiply(a, b = 1) {
+    return a * b;
+  }
+  
+  console.log(multiply(5, 2));
+  
+  console.log(multiply(10,5));
+  
+
+  function toCelsius(fahrenheit) {
+    return (5/9) * (fahrenheit-32);
+  }
+  console.log(toCelsius(56))
+  
