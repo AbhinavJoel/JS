7611ba4317b7c0cacce220d789168f6fee59ac28
AbhinavJoel <abhijoe20@gmail.com>
2020-09-02 12:52:31 +0530

    date added

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
-var  x = Math.pow(4, 2);
-console.log(x)
-x = Math.sqrt(100);
-console.log(x)
-x = Math.abs(9.82);  
-console.log(x)
-x = Math.random();  
-console.log(x)
-x = Math.ceil(9.8); 
-console.log(x)
-x = Math.round(6.3);
-console.log(x)
-x = Math.floor(8.9);  
-console.log(x)
-x = Math.sin(90 * Math.PI / 180);  
-console.log(x)
+var currentdate = new Date(); 
+var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+console.log(currentdate)
+console.log(datetime)
