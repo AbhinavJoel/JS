c9d4db7e2b522e58d1c718a39fe62294a1751f89
AbhinavJoel <abhijoe20@gmail.com>
2020-09-03 03:48:39 +0530

    str

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
-var a=10;
-var b=20;
-c=a/b;
-try{
-    if (c>1)
-    console.log(c)
-    else
-    throw c
-}
-catch(c){
-    console.log("div cannot be done")
-}
-finally{
-    console.log(c)
-}
+var str = "The fat cat sat on the man's black hat under the sun";
+var str2 = "Apple, Banana, Kiwi";
+arr = str.split(" ");
+console.log(arr[4])
+console.log(arr.slice(6,7))
+console.log(str2.slice(7,13))
