cf3fa640bf0de4423accf4d71fb3e8a8ab964af5
AbhinavJoel <abhijoe20@gmail.com>
2020-09-03 01:47:31 +0530

    fibo

--- main.js
+++ main.js
@@ -22,18 +22,18 @@ switch (x)
     break;
 }
 
-function fibonacci(num) 
-{ 
-    var num1=0; 
-    var num2=1; 
-    var sum; 
-    var i=0; 
-    for (i = 0; i < num; i++)  
-    { 
-        sum=num1+num2; 
-        num1=num2; 
-        num2=sum; 
-    } 
-    return num2; 
-} 
-document.write("Fibonacci(7): "+fibonacci(7)+"<br>"); 
+var fibonacci_series = function (n) 
+{
+  if (n===1) 
+  {
+    return [0, 1];
+  } 
+  else 
+  {
+    var s = fibonacci_series(n - 1);
+    s.push(s[s.length - 1] + s[s.length - 2]);
+    return s;
+  }
+};
+
+ console.log(fibonacci_series(8));
