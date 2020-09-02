82dd0913a931b7d8984efcef31de83c4f924b696
AbhinavJoel <abhijoe20@gmail.com>
2020-09-03 01:43:28 +0530

    loops and switch

--- main.js
+++ main.js
@@ -1,12 +1,39 @@
-function displayDate() {
-    document.getElementById("demo").innerHTML = Date()
+function ranges(x){
+    if (x>10 && x<100)
+    console.log("Its between one to hundred")
+    else
+    console.log("its not")
 }
 
-function myFunction() {
-    var x = document.getElementById("fname");
-    x.value = x.value.toUpperCase();
+ranges(25)
+
+var x = 50
+switch (x)
+{
+    case 50:
+        if (x>10 && x<100)
+        console.log( "is between one to hundred")
+        break;
+        
+    
+    default:
+        console.log("its not")
+        console.log(typeof x)
+    break;
 }
 
-function myFunction() {
-    document.getElementById("demo2").innerHTML = "Nice to meet you buddy";
-  }
+function fibonacci(num) 
+{ 
+    var num1=0; 
+    var num2=1; 
+    var sum; 
+    var i=0; 
+    for (i = 0; i < num; i++)  
+    { 
+        sum=num1+num2; 
+        num1=num2; 
+        num2=sum; 
+    } 
+    return num2; 
+} 
+document.write("Fibonacci(7): "+fibonacci(7)+"<br>"); 
