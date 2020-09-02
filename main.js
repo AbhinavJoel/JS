861c1f47e7aac8dbe660fd57aeb1f4bd7d56a1a0
AbhinavJoel <abhijoe20@gmail.com>
2020-09-03 02:21:16 +0530

    try catch throw finally

--- main.js
+++ main.js
@@ -1,39 +1,15 @@
-function ranges(x){
-    if (x>10 && x<100)
-    console.log("Its between one to hundred")
+var a=10;
+var b=20;
+c=a/b;
+try{
+    if (c>1)
+    console.log(c)
     else
-    console.log("its not")
+    throw c
 }
-
-ranges(25)
-
-var x = 50
-switch (x)
-{
-    case 50:
-        if (x>10 && x<100)
-        console.log( "is between one to hundred")
-        break;
-        
-    
-    default:
-        console.log("its not")
-        console.log(typeof x)
-    break;
+catch(c){
+    console.log("div cannot be done")
 }
-
-var fibonacci_series = function (n) 
-{
-  if (n===1) 
-  {
-    return [0, 1];
-  } 
-  else 
-  {
-    var s = fibonacci_series(n - 1);
-    s.push(s[s.length - 1] + s[s.length - 2]);
-    return s;
-  }
-};
-
- console.log(fibonacci_series(8));
+finally{
+    console.log(c)
+}
