3e9e4cf0891413f4c3b475af2d03e5b932fbd552
AbhinavJoel <abhijoe20@gmail.com>
2020-09-04 00:53:06 +0530

    hide

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-var str = "The fat cat sat on the man's black hat under the sun";
-var str2 = "Apple, Banana, Kiwi";
-arr = str.split(" ");
-console.log(arr[4])
-console.log(arr.slice(6,7))
-console.log(str2.slice(7,13))
+$( "#clickme" ).click(function() {
+    $( "#book" ).hide( "slow", function() {
+      alert( "Animation complete." );
+    });
+  });
+
+
+  for ( var i = 0; i < 5; i++ ) {
+    $( "<div>" ).appendTo( document.body );
+  }
+  $( "div" ).click(function() {
+    $( this ).hide( 2000, function() {
+      $( this ).remove();
+    });
+  });
